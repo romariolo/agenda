16340433252df7128c659991140e640e03b3fffe
Francisco Romário Rodrigues Lopes <romariolo6199@gmail.com>
2024-12-12 15:06:07 -0300

    Criação do projeto agenda online

---
+++ src/test/java/agenda_online/agenda/AgendaApplicationTests.java
@@ -0,0 +1,13 @@
+package agenda_online.agenda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
